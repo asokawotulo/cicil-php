ad3208bffdd78884d2d2dd097a55daf3ee3a7724
Asoka Wotulo <asokawotulo@yahoo.com>
2022-01-11 19:48:05 +0700

    docs: describe purchase order  status

--- src/Enums/PurchaseOrderStatusEnum.php
+++ src/Enums/PurchaseOrderStatusEnum.php
@@ -4,8 +4,31 @@ namespace Cicil\Enums;
 
 class PurchaseOrderStatusEnum
 {
+	/**
+	 * User has not completed the transaction
+	 * 
+	 * @var string
+	 */
 	const PENDING = 'pending';
+
+	/**
+	 * User has checked out the order. Waiting for user pay the downpayment
+	 * 
+	 * @var string
+	 */
 	const WAITING_DOWNPAYMENT = 'waiting_downpayment';
+
+	/**
+	 * User has completed the transaction
+	 * 
+	 * @var string
+	 */
 	const COMPLETED = 'completed';
+
+	/**
+	 * Transaction has been cancelled by merchant
+	 * 
+	 * @var string
+	 */
 	const CANCEL = 'cancel';
 }
